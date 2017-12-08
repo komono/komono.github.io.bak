fabf033f9ee76201c82ed5da9d18aede86bf3c34
ptpan <1091711534@qq.com>
2017-12-08 10:18:55 +0800

    sub

--- sw.js
+++ sw.js
@@ -78,8 +78,8 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(PRECACHE).then(cache => {
       return cache.add('offline.html')
-      .then(self.skipWaiting())
-      .catch(err => console.log(err))
+        .then(self.skipWaiting())
+        .catch(err => console.log(err))
     })
   )
 });
@@ -91,7 +91,7 @@ self.addEventListener('install', e => {
  *
  *  waitUntil(): activating ====> activated
  */
-self.addEventListener('activate',  event => {
+self.addEventListener('activate', event => {
   console.log('service worker activated.')
   event.waitUntil(self.clients.claim());
 });
@@ -111,9 +111,9 @@ self.addEventListener('fetch', event => {
 
   // Skip some of cross-origin requests, like those for Google Analytics.
   if (HOSTNAME_WHITELIST.indexOf(new URL(event.request.url).hostname) > -1) {
-    
+
     // Redirect in SW manually fixed github pages 404s on repo?blah 
-    if(shouldRedirect(event.request)){
+    if (shouldRedirect(event.request)) {
       event.respondWith(Response.redirect(getRedirectUrl(event.request)))
       return;
     }
@@ -123,7 +123,9 @@ self.addEventListener('fetch', event => {
     // Upgrade from Jake's to Surma's: https://gist.github.com/surma/eb441223daaedf880801ad80006389f1
     const cached = caches.match(event.request);
     const fixedUrl = getFixedUrl(event.request);
-    const fetched = fetch(fixedUrl, {cache: "no-store"});
+    const fetched = fetch(fixedUrl, {
+      cache: "no-store"
+    });
     const fetchedCopy = fetched.then(resp => resp.clone());
 
     // Call respondWith() with whatever we get first.
@@ -132,15 +134,15 @@ self.addEventListener('fetch', event => {
     // If neither yields a response, return offline pages.
     event.respondWith(
       Promise.race([fetched.catch(_ => cached), cached])
-        .then(resp => resp || fetched)
-        .catch(_ => caches.match('offline.html'))
+      .then(resp => resp || fetched)
+      .catch(_ => caches.match('offline.html'))
     );
 
     // Update the cache with the version we fetched (only for ok status)
     event.waitUntil(
       Promise.all([fetchedCopy, caches.open(RUNTIME)])
-        .then(([response, cache]) => response.ok && cache.put(event.request, response))
-        .catch(_ => {/* eat any errors */})
+      .then(([response, cache]) => response.ok && cache.put(event.request, response))
+      .catch(_ => { /* eat any errors */ })
     );
   }
 });
